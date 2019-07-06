47b37af161261c643bcf86e6962bd35fde916ae1
Lovepreet Singh <7azycoder@gmail.com>
2019-07-06 14:10:44 +0530

    save code

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
-o -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t engimo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
